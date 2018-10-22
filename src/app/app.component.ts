be4a651eaaee13d3dc4da20a362bf9f2ad95f3b6
myflowpl <pedro.blaszczak@gmail.com>
2018-10-22 15:29:43 +0200

    init angulara

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+
+  title = 'MapTodo';
+
 }
